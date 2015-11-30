79b939fc73b65a32bd6a4447746b853049445c0a
Roman Gayu <roman.gayu@gmail.com>
2015-11-30 16:09:38 +0300

    Carthage support

---
+++ SwiftPulse/SwiftPulse/SwiftPulse.h
@@ -0,0 +1,19 @@
+//
+//  SwiftPulse.h
+//  SwiftPulse
+//
+//  Created by Roman Gayu on 30/11/15.
+//  Copyright © 2015 RG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftPulse.
+FOUNDATION_EXPORT double SwiftPulseVersionNumber;
+
+//! Project version string for SwiftPulse.
+FOUNDATION_EXPORT const unsigned char SwiftPulseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftPulse/PublicHeader.h>
+
+
